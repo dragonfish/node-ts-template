3931c7be586ddb684cfcdcaa10d991674ead0b9c
Debkanchan Samadder <debu.samadder@gmail.com>
2023-06-17 11:51:53 +0530

    fix: format

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,11 +4,11 @@
  * @group unit
  */
 
-import greeter from '../src/index';
+import greeter from "../src/index";
 
 // Test for greeter function
-describe('greeter', () => {
-	it('should return a greeting', () => {
-		expect(greeter('test')).toBe('Hello, test!');
+describe("greeter", () => {
+	it("should return a greeting", () => {
+		expect(greeter("test")).toBe("Hello, test!");
 	});
 });
